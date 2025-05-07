44d59935e07618eb342ec18b137a524dcbaf5c78
root <youssefladeb@gmail.com>
2025-05-07 17:04:41 +0000

    made the middleman file and modified client to output the result coming from the server

--- client/main.c
+++ client/main.c
@@ -9,56 +9,54 @@
 #include <netdb.h>
 
 int main (int argc, char** argv) {
-	// Check that the program is being called correctly
-	if (argc != 3) {
-		fprintf(stderr, "Usage: %s [server ip] [server port]", argv[0]);
-		return -1;
-	}
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s [server ip] [server port]", argv[0]);
+        return -1;
+    }
 
-	// Check that the provided port is valid. There's no need to manually
-	// verify the IP address, as inet_pton does that on its own.
-	uint16_t server_port = atoi(argv[2]);
-	server_port = htons(server_port);	
+    uint16_t server_port = atoi(argv[2]);
+    server_port = htons(server_port);
 
-	int sock = socket(AF_INET, SOCK_STREAM, getprotobyname("tcp")->p_proto);
-	if (sock < 0) {
-		perror("Could not create socket");
-		return -1;
-	}
+    int sock = socket(AF_INET, SOCK_STREAM, getprotobyname("tcp")->p_proto);
+    if (sock < 0) {
+        perror("Could not create socket");
+        return -1;
+    }
 
-	struct sockaddr_in server_addr = {0};
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = server_port;
-	if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) == 0) {
-		fprintf(stderr, "Please provide a valid IP address.");
-		return -1;
-	}
+    struct sockaddr_in server_addr = {0};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = server_port;
+    if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) == 0) {
+        fprintf(stderr, "Please provide a valid IP address.");
+        return -1;
+    }
 
-	if (connect(sock, (struct sockaddr*) &server_addr, sizeof(server_addr)) != 0) {
-		perror("Could not connect to server");
-		return -1;
-	}
+    if (connect(sock, (const struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+        perror("Could not connect to server");
+        return -1;
+    }
 
-	// Getting arithmetic operation from input
-	float op1, op2;
-	char operator = 0, req[256] = {0}, res[256] = {0};
-	
-	printf("First operand? ");
-	scanf("%f", &op1);
+    float op1, op2;
+    char operator;
+    if (scanf("[ %f , %f , %c ]", &op1, &op2, &operator) != 3) {
+        fprintf(stderr, "Unrecognized pattern. Expected input: [operand1, operand2, operator].\n");
+        return -1;
+    }
 
-	printf("Second operand? ");
-	scanf("%f", &op2);
+    printf("%f %c %f\n", op1, operator, op2);
 
-	printf("Operator? [+, -, *, /] ");
-	scanf(" %c", &operator);
+    // === New code: format and send the message to the server ===
+    char message[100];
+    snprintf(message, sizeof(message), "%f,%f,%c", op1, op2, operator);
+    send(sock, message, strlen(message), 0);
 
-	snprintf(req, sizeof(req), "[%f, %f, %c]", op1, op2, operator);
+    // === New code: receive the response from the server ===
+    char buffer[1024] = {0};
+    recv(sock, buffer, sizeof(buffer), 0);
+    printf("Result from server: %s\n", buffer);
 
-	send(sock, req, strlen(req), 0);
+    // Close the socket
+    close(sock);
 
-	recv(sock, res, sizeof(res), 0);
-	printf("Server response: %s\n", res);
-
-	return 0;
+    return 0;
 }
-
---
+++ middleman/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define MIDDLEMAN_PORT 3333
+#define BUFFER_SIZE 1024
+#define CORRUPTION_PROBABILITY 10  // 10% chance
+
+void corrupt_message(char *msg, int length) {
+    if (length <= 1) return;  // Don't corrupt empty messages or just checksum
+
+    // Choose a random position (excluding the checksum byte)
+    int pos = rand() % (length - 1);
+
+    // Perform simple corruption (flip one bit)
+    msg[pos] ^= 0x01;  // XOR with 00000001
+
+    printf("Middleman corrupted byte at position %d (changed to 0x%02X)\n", pos, (unsigned char)msg[pos]);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s [server ip] [server port]\n", argv[0]);
+        exit(1);
+    }
+
+    int sockfd;
+    struct sockaddr_in middleaddr, servaddr, cliaddr;
+    char buffer[BUFFER_SIZE];
+
+    // Initialize better random seed
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    srand((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+
+    // Create socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(1);
+    }
+
+    // Configure middleman address
+    memset(&middleaddr, 0, sizeof(middleaddr));
+    middleaddr.sin_family = AF_INET;
+    middleaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    middleaddr.sin_port = htons(MIDDLEMAN_PORT);
+
+    // Bind to middleman port
+    if (bind(sockfd, (const struct sockaddr *)&middleaddr, sizeof(middleaddr)) < 0) {
+        perror("bind failed");
+        exit(1);
+    }
+
+    // Configure server address
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(atoi(argv[2]));
+    servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    printf("Middleman running on port %d (10%% corruption chance)...\n", MIDDLEMAN_PORT);
+
+    while (1) {
+        socklen_t addr_len = sizeof(cliaddr);
+        int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
+                        (struct sockaddr *)&cliaddr, &addr_len);
+
+        // Only corrupt with 10% probability
+        if (rand() % 100 < CORRUPTION_PROBABILITY) {
+            corrupt_message(buffer, n);
+        }
+
+        // Forward to server
+        sendto(sockfd, buffer, n, 0,
+              (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+        // Forward response back to client
+        n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
+                    (struct sockaddr *)&servaddr, &addr_len);
+        sendto(sockfd, buffer, n, 0,
+              (const struct sockaddr *)&cliaddr, addr_len);
+    }
+
+    close(sockfd);
+    return 0;
+}
