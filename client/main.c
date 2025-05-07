43725e276e33b1385d57a19058a3d6865afa0e35
root <youssefladeb@gmail.com>
2025-05-07 17:24:17 +0000

    removed the executables

--- client/main.c
+++ client/main.c
@@ -45,17 +45,14 @@ int main (int argc, char** argv) {
 
     printf("%f %c %f\n", op1, operator, op2);
 
-    // === New code: format and send the message to the server ===
     char message[100];
     snprintf(message, sizeof(message), "%f,%f,%c", op1, op2, operator);
     send(sock, message, strlen(message), 0);
 
-    // === New code: receive the response from the server ===
     char buffer[1024] = {0};
     recv(sock, buffer, sizeof(buffer), 0);
     printf("Result from server: %s\n", buffer);
 
-    // Close the socket
     close(sock);
 
     return 0;
